6215021d19d4441f4ae01f97eec18f445bbf3993
SnowballSH <derek070211@gmail.com>
2021-04-14 20:48:45 -0500

    first commit

---
+++ assets/js/cite.js
@@ -0,0 +1,14 @@
+window.onload = () => {
+  let url = getQueryVariable("url");
+  let title = getQueryVariable("title");
+  let pub = getQueryVariable("pub");
+  let first = getQueryVariable("first");
+  let last = getQueryVariable("last");
+
+  let t1 = `${cap(last)}, ${cap(first)}. <i>${title}</i>.`;
+  let tb = pub ? `${pub}` : "";
+  let t2 = url ? `, <a href="${url}">${url}</a>` : "";
+
+  document.getElementById("area").innerHTML =
+    "<b>Your MLA8 cited text:</b><br>" + t1 + tb + t2 + ".";
+};
---
+++ assets/js/main.js
@@ -0,0 +1,26 @@
+function cite() {
+  window.location.href = `/cite.html?url=${get("URL")}&title=${get(
+    "Title"
+  )}&pub=${get("Publisher")}&first=${get("First")}&last=${get("Last")}`;
+}
+
+function get(name) {
+  return encodeURIComponent(document.getElementsByName(name)[0].value);
+}
+
+function getQueryVariable(variable) {
+  var query = window.location.search.substring(1);
+  var vars = query.split("&");
+  for (var i = 0; i < vars.length; i++) {
+    var pair = vars[i].split("=");
+    if (decodeURIComponent(pair[0]) == variable) {
+      return decodeURIComponent(pair[1]);
+    }
+  }
+  console.error("Query variable %s not found", variable);
+}
+
+function cap(s) {
+  if (typeof s !== "string") return "";
+  return s.charAt(0).toUpperCase() + s.slice(1);
+}
